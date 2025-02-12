960ed6d688e585eecf56a150f13b73b1ab9bcd25
Vishwa GW <136871744+vishwagw@users.noreply.github.com>
2025-02-12 08:05:56 +0530

    Update object_detect.py

--- object_detect.py
+++ object_detect.py
@@ -1,1 +1,11 @@
+# for detecting in videos:
+import cv2
+from ultralytics import YOLO
 
+# model load:
+y_model = YOLO('./model/yolov8n.pt')
+
+# video path:
+#video_path = './input/input4.mp4'
+# load video
+cap = cv2.VideoCapture(1)
